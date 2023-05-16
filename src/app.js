67cc799617079986de6b7e0dfd22b4ae9e0b3be2
Marina Master <marina777master@mail.ru>
2023-05-15 22:17:48 -0400

    Complete generator

--- src/app.js
+++ src/app.js
@@ -1,23 +1,31 @@
 /* eslint-disable */
+import "bootstrap";
+import "./style.css";
 
+import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  let randomNumber = Math.random() * 10;
-  document.querySelector =('#excuse').innerHTML = generateExcuse();
-  console.log("Hello Rigo from the console!");
+  document.querySelector(
+    "#excuse"
+  ).innerHTML = `<strong>${generateExcuse()}</strong>`;
 };
 
-let generateExcuse = () => {
-  let who = ['The dog','My grandma','His turtle','My bird'];
-  let action = ['ate','peed','crushed','broke'];
-  let what = ['my homework', 'the keys', 'the car'];
-  let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
-  
-  
-  let exWho = Math.round(Math.random() * who.length);
-  let exAction = Math.round(Math.random() * action.length);
-  let exWhat = Math.round(Math.random() * what.length);
-  let exWhen = Math.round(Math.random() * when.length);
-  
-  return who[exWho] + ' ' + action[exAction] + ' ' + what[exWhat] + ' ' + when[exWhen]
-  };
+const generateExcuse = () => {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const exWho = Math.floor(Math.random() * who.length);
+  const exAction = Math.floor(Math.random() * action.length);
+  const exWhat = Math.floor(Math.random() * what.length);
+  const exWhen = Math.floor(Math.random() * when.length);
+
+  return `${who[exWho]} ${action[exAction]} ${what[exWhat]} ${when[exWhen]}`;
+};
